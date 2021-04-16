f822f7c7613778d6d85a9d71e0bd5be922414da5
Cairo Lopes <cairosilvalopes23@gmail.com>
2021-04-16 11:16:07 -0300

    add put_thing function

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import logging
 import sys
+import os
 from minioConn import MinioSDK
 
 minio = MinioSDK()
-minio.mk_bucket(minio.minioClient(), "buckerest")
+bucket = "kaggle"
+directory = "data/transaction_dataset.csv"
+file_name = "transaction_dataset.csv"
+
+# minio.mk_bucket(minio.minioClient(), "buckerest")
+
+minio.put_thing(
+    minio.minioClient(),
+    bucket,
+    file_name,
+    directory
+)
+
--- minioConn.py
+++ minioConn.py
@@ -1,5 +1,6 @@
-import logging
+import os
 import sys
+import logging
 
 from minio import Minio 
 from minio.error import S3Error
@@ -23,7 +24,7 @@ class MinioSDK:
             region=self.region
         )
 
-    def mk_bucket(self, client, bucket_name: str):
+    def mk_bucket(self, client, bucket_name: str) -> None:
 
         # Make new_bucket if not exist.
         found = client.bucket_exists(bucket_name)
@@ -35,3 +36,31 @@ class MinioSDK:
                 print("".join(['ERROR OCCURRED.', str(exc)]))
         else:
             print('Bucket {} already exists'.format(bucket_name))
+    
+    # TODO: create a function to list all buckets
+    def bucket_list(self) -> list:
+        pass 
+    
+    # TODO: create a function to increment things (data) in the bucket 
+    def put_thing(self, client, bucket_name: str, file_name: str, directory: str) -> None:
+
+        found = client.bucket_exists(bucket_name)
+
+        if found:
+            try:
+                client.fput_object(
+                    bucket_name,
+                    file_name,
+                    directory
+                )
+                
+                print(f'{file_name} is successfully uploaded')
+
+            except S3Error as exc:
+                print("".join(['ERROR OCCURRED.', str(exc)]))
+            
+
+        else:
+
+            print('Bucket {} already exists'.format(bucket_name))
+
